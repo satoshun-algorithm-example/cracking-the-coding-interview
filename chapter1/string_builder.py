54e898c1a0ed8ff50cdddcce364a1aa0fb1943d3
SatoShun <shun.sato1@gmail.com>
2019-05-05 07:48:08 +0000

    int size

--- chapter1/string_builder.py
+++ chapter1/string_builder.py
@@ -7,19 +7,32 @@ class StringBuilder:
 
     def append(self, s):
         if isinstance(s, int):
-            return
+            return self._append_int(s)
         if isinstance(s, str):
             return self._append_string(s)
         if s is None:
             return self._append_none()
         raise Exception("not implemented type " + str(type(s)))
 
+    def _append_int(self, i):
+        if i is None:
+            return self._append_none()
+
+        size = Int.string_size(i)
+        if i < 0:
+            size += 1
+        self._ensureCapacityInternal(self.count + size)
+        Int.get_chars(i, self.count + size, self.value)
+        self.count += size
+
+        return self
+
     def _append_string(self, s):
         if s is None:
             return self._append_none()
         l = len(s)
         self._ensureCapacityInternal(self.count + l)
-        String.getChars(
+        String.get_chars(
             s,
             0,
             l,
@@ -56,14 +69,52 @@ class StringFactory:
 
 class String:
     @staticmethod
-    def getChars(s, begin, end, dst, dst_begin):
+    def get_chars(s, begin, end, dst, dst_begin):
         for j, i in enumerate(range(begin, end)):
             dst[dst_begin+j] = s[i]
 
-builder = StringBuilder(16)
+class Int:
+    size_table = [9, 99, 999, 9999, 99999]
+
+    @staticmethod
+    def string_size(x):
+        if x < 0:
+            x = -x
+        for i in range(10000):
+            if Int.size_table[i] >= x:
+                return i + 1
+
+    @staticmethod
+    def get_chars(i, index, dst):
+        sign = 0
+        if i < 0:
+            sign = '-'
+            i = -i
+
+        index -= 1
+        while True:
+            q = i // 10
+            digit = i - q * 10
+            dst[index] = str(digit)
+            index -= 1
+            i = q
+
+            if q == 0:
+                break
+
+        if sign != 0:
+            dst[index] = sign
+
+builder = StringBuilder(32)
 
 builder.append(None)
 assert builder.toString() == 'none'
 
-builder.append(' test')
-assert builder.toString() == 'none test'
+builder.append(' test ')
+assert builder.toString() == 'none test '
+
+builder.append(123)
+assert builder.toString() == 'none test 123'
+
+builder.append(-987)
+assert builder.toString() == 'none test 123-987'
