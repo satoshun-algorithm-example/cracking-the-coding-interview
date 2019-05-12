7c5121de0bb1e8a1507af60f7b63dd6145e6cf46
SatoShun <shun.sato1@gmail.com>
2019-05-12 22:58:00 +0000

    q1-4 palindrome2

--- chapter1/4/main.go
+++ chapter1/4/main.go
@@ -21,7 +21,44 @@ func palindrome(s string) bool {
 	return true
 }
 
+func palindrome2(s string) bool {
+	var index = 0
+	for _, c := range s {
+		var i = uint8(c - 'a')
+		var j = 1 << i
+		index ^= j
+	}
+
+	var permit = 0
+	if len(s)%2 == 1 {
+		permit = 1
+	}
+
+	for {
+		if index <= 0 {
+			break
+		}
+
+		var b = index & 1
+		if b == 1 {
+			if permit == 0 {
+				return false
+			}
+			permit -= 1
+		}
+		index = index >> 1
+	}
+
+	return true
+}
+
 func main() {
-	fmt.Println(palindrome("tactcoa") == true)
-	fmt.Println(palindrome("tactco") == false)
+	{
+		fmt.Println(palindrome("tactcoa") == true)
+		fmt.Println(palindrome("tactco") == false)
+	}
+	{
+		fmt.Println(palindrome2("tactcoa") == true)
+		fmt.Println(palindrome2("tactco") == false)
+	}
 }
