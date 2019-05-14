226a83735b4aea28cb11ad92dd2d2afd92099a73
SatoShun <shun.sato1@gmail.com>
2019-05-14 00:29:42 +0000

    q1-5 oneshot 2

--- chapter1/5/main.go
+++ chapter1/5/main.go
@@ -36,9 +36,52 @@ func oneShotTranslate(s1 string, s2 string) bool {
 	return false
 }
 
+func oneShotTranslate2(s1 string, s2 string) bool {
+	if len(s1) < len(s2) {
+		tmp := s2
+		s2 = s1
+		s1 = tmp
+	}
+
+	var i1 = 0
+	var i2 = 0
+	var matched = false
+	for {
+		if i1 == len(s1) {
+			break
+		}
+		if i2 == len(s2) {
+			break
+		}
+
+		if s1[i1] != s2[i2] {
+			if matched {
+				return false
+			}
+			if len(s1) == len(s2) {
+				i2++
+			}
+			matched = true
+		} else {
+			i2++
+		}
+		i1++
+	}
+	return true
+}
+
 func main() {
-	fmt.Println(oneShotTranslate("pale", "ple") == true)
-	fmt.Println(oneShotTranslate("pales", "pale") == true)
-	fmt.Println(oneShotTranslate("pale", "bale") == true)
-	fmt.Println(oneShotTranslate("pale", "bake") == false)
+	{
+		fmt.Println(oneShotTranslate("pale", "ple") == true)
+		fmt.Println(oneShotTranslate("pales", "pale") == true)
+		fmt.Println(oneShotTranslate("pale", "bale") == true)
+		fmt.Println(oneShotTranslate("pale", "bake") == false)
+	}
+
+	{
+		fmt.Println(oneShotTranslate2("pale", "ple") == true)
+		fmt.Println(oneShotTranslate2("pales", "pale") == true)
+		fmt.Println(oneShotTranslate2("pale", "bale") == true)
+		fmt.Println(oneShotTranslate2("pale", "bake") == false)
+	}
 }
