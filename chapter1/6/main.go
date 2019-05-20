78ffbcdf267c5b9bb0d182dbe8b7fb23c2184730
SatoShun <shun.sato1@gmail.com>
2019-05-20 00:39:57 +0000

    q1-6 compression string use strings.Builder

--- chapter1/6/main.go
+++ chapter1/6/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func stringCompression(s string) string {
@@ -30,6 +31,39 @@ func stringCompression(s string) string {
 	return cs
 }
 
+func stringCompression2(s string) string {
+	var cs strings.Builder
+	var b rune
+	var i int = 0
+
+	for _, c := range s {
+		if b != c {
+			if i == 0 {
+				b = c
+				i = 1
+				continue
+			}
+			cs.WriteRune(b)
+			cs.WriteString(strconv.Itoa(i))
+
+			b = c
+			i = 1
+			continue
+		}
+		i++
+	}
+
+	cs.WriteRune(b)
+	cs.WriteString(strconv.Itoa(i))
+	return cs.String()
+}
+
 func main() {
-	fmt.Println(stringCompression("aabcccccaaa") == "a2b1c5a3")
+	{
+		fmt.Println(stringCompression("aabcccccaaa") == "a2b1c5a3")
+	}
+
+	{
+		fmt.Println(stringCompression2("aabcccccaaa") == "a2b1c5a3")
+	}
 }
