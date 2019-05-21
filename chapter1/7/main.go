c572e303545743b1011e44bc5e93b6966fdbb952
SatoShun <shun.sato1@gmail.com>
2019-05-21 23:26:44 +0000

    q1-7 two dimensional answer

--- chapter1/7/main.go
+++ chapter1/7/main.go
@@ -16,9 +16,40 @@ func maxtrixRotation(a [][]int) [][]int {
 	return b
 }
 
+func maxtrixRotation2(a [][]int) [][]int {
+	n := len(a)
+	for layer := 0; layer < n/2; layer++ {
+		first := layer
+		last := n - 1 - layer
+		for i := first; i < last; i++ {
+			offset := i - first
+			top := a[first][i]
+
+			a[first][i] = a[last-offset][first]
+			a[last-offset][first] = a[last][last-offset]
+			a[last][last-offset] = a[i][last]
+			a[i][last] = top
+		}
+	}
+
+	for _, bb := range a {
+		fmt.Println(bb)
+	}
+	return a
+}
+
 func main() {
 	{
 		maxtrixRotation([][]int{
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+			{13, 14, 15, 16},
+		})
+	}
+	fmt.Println()
+	{
+		maxtrixRotation2([][]int{
 			{1, 2, 3, 4},
 			{5, 6, 7, 8},
 			{9, 10, 11, 12},
