d34030d464da72348a06cf69eb08f6f2b25fba12
SatoShun <shun.sato1@gmail.com>
2019-05-31 00:17:35 +0000

    add q1-8

--- chapter1/8/main.go
+++ chapter1/8/main.go
@@ -32,6 +32,43 @@ func fillZero(matrix [][]int) {
 	}
 }
 
+func fillZero2(matrix [][]int) {
+	column := len(matrix)
+	row := len(matrix[0])
+	for i := 0; i < row; i++ {
+		for j := 0; j < column; j++ {
+			if matrix[i][j] == 0 {
+				matrix[i][0] = -1
+			}
+		}
+	}
+	for i := 0; i < column; i++ {
+		for j := 0; j < row; j++ {
+			if matrix[j][i] == 0 {
+				matrix[0][i] = -1
+			}
+		}
+	}
+	for i := 0; i < row; i++ {
+		if matrix[i][0] == -1 {
+			for j := 0; j < column; j++ {
+				matrix[i][j] = 0
+			}
+		}
+	}
+	for i := 0; i < column; i++ {
+		if matrix[0][i] == -1 {
+			for j := 0; j < row; j++ {
+				matrix[j][i] = 0
+			}
+		}
+	}
+
+	for i, _ := range matrix {
+		fmt.Println(matrix[i])
+	}
+}
+
 func main() {
 	{
 		fillZero([][]int{
@@ -42,4 +79,12 @@ func main() {
 		})
 	}
 	fmt.Println()
+	{
+		fillZero2([][]int{
+			{1, 2, 3, 4},
+			{5, 6, 0, 8},
+			{9, 0, 11, 12},
+			{13, 14, 15, 16},
+		})
+	}
 }
